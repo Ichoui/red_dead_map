8739728fca5b09621ad4d35a33309e89c471c6fd
Morgan <nicol.morgan@outlook.fr>
2018-11-21 16:38:27 +0100

    Ca marche du feu de dieu : structure du javascript, commentaires, fonction pour les markers et groupes

--- public/resources/js/map.js
+++ public/resources/js/map.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    // #####################  MAP DECLARATION  #####################
     // declare Map object
     let map = L.map('map').setView([0, 0], 3);
     L.tileLayer('/resources/img/map/{z}/{x}/{y}.png', {
@@ -17,69 +19,62 @@ $(document).ready(function () {
         map.unproject(mapNE, map.getMaxZoom())
     ));
 
-    // icons
-    let collectibleTest = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [24, 24]});
-    let iconToto = L.icon({iconUrl: '/resources/img/thomas-marker.png', iconSize: [24, 24]});
-    let iconPetit = L.icon({iconUrl: '/resources/img/morgan-marker.png', iconSize: [24, 24]});
-    let iconGG = L.icon({iconUrl: '/resources/img/gaetan-marker.png', iconSize: [24, 24]});
+    // #####################  ICONS  #####################
+    window.icons = {};
+    let icons = window.icons;
 
-    // #####################   MARKERS  #####################
+    icons.collectibleTest = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [24, 24]});
+    icons.iconToto = L.icon({iconUrl: '/resources/img/thomas-marker.png', iconSize: [24, 24]});
+    icons.iconPetit = L.icon({iconUrl: '/resources/img/morgan-marker.png', iconSize: [24, 24]});
+    icons.iconGG = L.icon({iconUrl: '/resources/img/gaetan-marker.png', iconSize: [24, 24]});
+
+    let iconsLength = Object.keys(icons).length; // obtenir length du json array icons
+
+    // #####################  MARQUEURS DEVELOPPEUR  #####################
     //// 8192 8192 = milieu parfait en pixel
-    let markerPixel = L.marker(map.unproject([6500, 8192], map.getMaxZoom()), {icon: collectibleTest, draggable: true}).addTo(map).bindPopup();
-    $(markerPixel).on('dragend', function () {
-        markerPixel.getPopup().setContent('Marqueur Tutoriel <br>' +
-            'Coord : ' + markerPixel.getLatLng().toString() + '<br>' +
-            'Pixels : ' + map.project(markerPixel.getLatLng(), map.getMaxZoom().toString())).openOn(map);
-        console.log(markerThomas.getLatLng().toString());
-        console.log('Pixels : ' + map.project(markerThomas.getLatLng(), map.getMaxZoom().toString()))
-    });
-
-
-    /////////////////////////////////////////////////////////////////
-    // marqueurs perso pour placer l'élément ?
-    // Un pour chaque dev avec couleurs specs
-    let markerMorgan = L.marker(map.unproject([8192, 8192], map.getMaxZoom()), {icon: iconPetit, draggable: true}).addTo(map).bindPopup('Marqueur Morgan');
+    let markerMorgan = L.marker(map.unproject([8192, 8192], map.getMaxZoom()), {icon: icons.iconPetit, draggable: true}).addTo(map).bindPopup('Marqueur Morgan');
     $(markerMorgan).on('dragend', function (e) {
         console.log(markerMorgan.getLatLng().toString());
         console.log('Pixels : ' + map.project(markerMorgan.getLatLng(), map.getMaxZoom().toString()))
     });
 
-    let markerGaetan = L.marker(map.unproject([5000, 5000], map.getMaxZoom()), {icon: iconGG, draggable: true}).addTo(map).bindPopup('Marqueur Gaetan');
+    let markerGaetan = L.marker(map.unproject([5000, 5000], map.getMaxZoom()), {icon: icons.iconGG, draggable: true}).addTo(map).bindPopup('Marqueur Gaetan');
     $(markerGaetan).on('dragend', function (e) {
         console.log(markerGaetan.getLatLng().toString());
         console.log('Pixels : ' + map.project(markerGaetan.getLatLng(), map.getMaxZoom().toString()))
     });
 
-    let markerThomas = L.marker(map.unproject([6000, 6000], map.getMaxZoom()), {icon: iconToto, draggable: true}).addTo(map).bindPopup('Marqueur Thomas');
+    let markerThomas = L.marker(map.unproject([6000, 6000], map.getMaxZoom()), {icon: icons.iconToto, draggable: true}).addTo(map).bindPopup('Marqueur Thomas');
     $(markerThomas).on('dragend', function (e) {
         console.log(markerThomas.getLatLng().toString());
         console.log('Pixels : ' + map.project(markerThomas.getLatLng(), map.getMaxZoom().toString()))
     });
 
 
-    //////////////////////////////////////////////////////////////////////// On sépare, on verra + tard
-    window.icons = {};
-    window.markers = {};
-    let icons = window.icons;
-    let markers = window.markers;
-
-    icons.collectibleTest = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [38, 95]});
-    icons.collectdouble = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [38, 95]});
-    let iconsLength = Object.keys(icons).length; // obtenir length du json array icons
+    // #####################  GROUPES DE COLLECTIBLES #####################
+    var os = L.layerGroup([]);
+    var sculptures = L.layerGroup([]);
 
 
-// mapping json to array
-    let arr = $.map(icons, function (el) {
-        return el
-    });
+    // #####################  MARKERS COLLECTIBLES  #####################
 
-    for (let i = 0; i < iconsLength; i++) {
-        // console.log(arr[i].options);
-        let x = arr[i].options.iconSize[0];
-        let y = arr[i].options.iconSize[1];
-        let url = arr[i].options.iconUrl;
-        // console.log(L.marker([x, y]).addTo(map));
+    function customMarker(x, y, icon, group, message) {
+        console.log(icon);
+        L.marker(map.unproject([x, y], map.getMaxZoom()), {icon: icon}).addTo(group).bindPopup(message);
     }
 
+    customMarker(11225,12245, icons.iconPetit, os, "aaaaaa");
+    customMarker(11225,8500, icons.iconPetit, sculptures, "aaaaaa");
+
+    // L.marker(map.unproject([11225, 7004], map.getMaxZoom()), {icon: icons.collectibleTest}).addTo(sculptures).bindPopup('sculpture');
+    // L.marker(map.unproject([10220, 7004], map.getMaxZoom()), {icon: icons.collectibleTest}).addTo(os).bindPopup('Mon os');
+
+
+    // #####################  AJOUTER LES GROUPES A LA MAP  #####################
+    var overlays = {
+        "Os de dinosaures": os,
+        "Sculptures": sculptures
+    };
+    L.control.layers(null, overlays).addTo(map);
 
 });
--- public/resources/js/page.js
+++ public/resources/js/page.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
     // Remove ads
     $('a[href="http://leafletjs.com"]').closest('.leaflet-bottom').remove();
+
+    // $('.leaflet-control-container').hide().clone();
 });
