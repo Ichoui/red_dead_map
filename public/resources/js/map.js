ffb28fd21c0f0def9c003ae94bba72073c084265
Morgan <nicol.morgan@outlook.fr>
2018-11-21 15:00:06 +0100

    On passe en pixel !

--- public/resources/js/map.js
+++ public/resources/js/map.js
@@ -1,23 +1,55 @@
 $(document).ready(function () {
+    let mapSW = [0, 16128],
+        mapNE = [16128, 0];
+
     // declare Map object
     let map = L.map('map').setView([0, 0], 3);
     L.tileLayer('/resources/img/map/{z}/{x}/{y}.png', {
         minZoom: 3,
         maxZoom: 6,
         noWrap: true,
+        crs: L.CRS.simple
 
     }).addTo(map);
 
+    map.setMaxBounds(new L.LatLngBounds(
+        map.unproject(mapSW, map.getMaxZoom()),
+        map.unproject(mapNE, map.getMaxZoom())
+    ));
+
+    // icons
     var collectibleTest = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [24, 24]});
+
+    // markers
+    // 0 0 = milieu parfait en latlng
     var marker = L.marker([0, 0], {icon: collectibleTest}).addTo(map);
     marker.bindPopup();
-
-    marker.on('click', function (e) {
-        console.log(marker.getLatLng().toString());
+    $(marker).on('dragend', function () {
         marker.getPopup().setContent('Coord : ' + marker.getLatLng().toString()).openOn(map);
     });
 
+    //// 8192 8192 = milieu parfait en pixel
+    var markerPixel = L.marker(map.unproject([8192, 8192], map.getMaxZoom()), {icon: collectibleTest, draggable: true}).addTo(map);
+    markerPixel.bindPopup();
+    $(markerPixel).on('dragend', function () {
+        markerPixel.getPopup().setContent('Coord : ' + markerPixel.getLatLng().toString() + '<br>'+
+        'Pixels : ' + map.project(markerPixel.getLatLng(), map.getMaxZoom().toString())).openOn(map);
+    });
 
+
+
+    /////////////////////////////////////////////////////////////////
+    // marqueurs perso pour placer l'élément ?
+    // Un pour chaque dev avec couleurs specs
+    var markerMorgan = L.marker([50, 0], {icon: collectibleTest, draggable: true}).addTo(map);
+    markerMorgan.bindPopup('Marqueur Morgan');
+
+    $(markerMorgan).on('dragend', function (e) {
+        console.log(markerMorgan.getLatLng().toString());
+        console.log('Pixels : ' + map.project(markerMorgan.getLatLng(), map.getMaxZoom().toString()))
+    });
+
+    //////////////////////////////////////////////////////////////////////// On sépare, on verra + tard
     window.icons = {};
     window.markers = {};
     var icons = window.icons;
