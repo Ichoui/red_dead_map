03e0fca21c88dd2fcf9043c28bbd66601ec0fb6d
Morgan <nicol.morgan@outlook.fr>
2018-11-21 13:16:58 +0100

    On avance comme qu'on peut

--- public/resources/js/fullpage.js
+++ public/resources/js/fullpage.js
@@ -1,84 +1,147 @@
-$(document).ready(function () {
-    /*
-     * = PS_Bramus.GoogleMapsTileCutter Config
-     * ----------------
-     */
-    //
-    // var repeatOnXAxis = false   ; // Do we need to repeat the image on the X-axis? Most likely you'll want to set this to false
-
-    /*
-     * Helper function which normalizes the coords so that tiles can repeat across the X-axis (horizontally) like the standard Google map tiles.
-     * ----------------
-     */
-
-    function getNormalizedCoord(coord, zoom) {
-        if (!(true)) return coord;
-
-        var y = coord.y;
-        var x = coord.x;
-
-        // tile range in one direction range is dependent on zoom level
-        // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
-        var tileRange = 1 << zoom;
-
-        // don't repeat across Y-axis (vertically)
-        if (y < 0 || y >= tileRange) {
-            return null;
-        }
-
-        // repeat across X-axis
-        if (x < 0 || x >= tileRange) {
-            x = (x % tileRange + tileRange) % tileRange;
-        }
-
-        return {
-            x: x,
-            y: y
-        };
-
-    }
-
-
-    /*
-     * Main Core
-     * ----------------
-     */
-
-    window.onload = function () {
-
-        // Define our custom map type
-        var customMapType = new google.maps.ImageMapType({
-            getTileUrl: function (coord, zoom) {
-                var normalizedCoord = getNormalizedCoord(coord, zoom);
-                if (normalizedCoord && (normalizedCoord.x < Math.pow(2, zoom)) && (normalizedCoord.x > -1) && (normalizedCoord.y < Math.pow(2, zoom)) && (normalizedCoord.y > -1)) {
-                    return "/resources/img/map/" + zoom + '/' + normalizedCoord.x + '/' + normalizedCoord.y + '.png';
-                } else {
-                    return '/resources/img/map/empty.png';
-                }
-            },
-            tileSize: new google.maps.Size(256, 256),
-            maxZoom: 6,
-            name: 'PS_Bramus.GoogleMapsTileCutter'
+L.Icon.Default.imagePath = '../files/images/leaflet';
+var getMapdata = function (mapname) {
+    $.cachedScript("../files/scripts/mapdata-" + mapname + ".js").done(function (script, textStatus) {
+        $.cachedScript("../files/scripts/custom.js").done(function (script, textStatus) {
+            $(document).i18n();
         });
-
-        // Basic options for our map
-        var myOptions = {
-            center: new google.maps.LatLng(0, 0),
-            zoom: 2,
-            minZoom: 0,
-            streetViewControl: false,
-            mapTypeControl: false,
-            mapTypeControlOptions: {
-                mapTypeIds: ["custom"]
-            }
-        };
-
-        // Init the map and hook our custom map type to it
-        var map = new google.maps.Map(document.getElementById('map'), myOptions);
-        map.mapTypes.set('custom', customMapType);
-        map.setMapTypeId('custom');
-
-    }
-
+    });
+};
+$.i18n.init(i18noptions, function () {
+    var namespace = location.pathname.match(/\/(\w{1})\/(?:index.html)?$/)[1];
+    $.i18n.loadNamespace(namespace, function () {
+        if (namespace == "w") {
+            getMapdata('white_orchard');
+        } else if (namespace == "v") {
+            getMapdata('hos_velen');
+        } else if (namespace == "s") {
+            getMapdata('skellige');
+        } else if (namespace == "t") {
+            getMapdata('toussaint');
+        } else if (namespace == "k") {
+            getMapdata('kaer_morhen');
+        }
+    });
 });
-
+$(function () {
+    $('.dd-selected').on('click', function () {
+        setTimeout(function () {
+            $("#sidebar").getNiceScroll().resize();
+        }, 500);
+    });
+});
+window.createMarker = function (coord, icon, label, popup, dataKey) {
+    var mapKey = 'markers-' + map_path + '-hidden';
+    var marker = L.marker(coord, setMarker(icon)).bindLabel(label, {direction: 'auto'}).bindPopup(popup);
+    marker.on('contextmenu', function (e) {
+        toggleOpacity(e, mapKey);
+        updatePills(e, dataKey);
+    });
+    if (isMarkerInvisible(mapKey, marker.getLatLng().lat, marker.getLatLng().lng)) {
+        marker.setOpacity(invisibleMarkerOpacity);
+        if (!markerCount[dataKey]) markerCount[dataKey] = 0;
+    } else {
+        markerCount[dataKey] = (markerCount[dataKey] + 1) || 1;
+    }
+    return marker;
+};
+window.setMarker = function (icon, tooltip) {
+    return {icon: icon, riseOnHover: true};
+};
+window.getLatLngKey = function (lat, lng) {
+    return lat + ';' + lng;
+};
+window.isMarkerInvisible = function (mapPath, lat, lng) {
+    return invisibleMarkers[mapPath].indexOf(getLatLngKey(lat, lng)) > -1;
+};
+window.toggleOpacity = function (event, mapPath) {
+    var key = getLatLngKey(event.latlng.lat, event.latlng.lng);
+    if (event.target && event.target.options.opacity === 1.0) {
+        event.target.setOpacity(invisibleMarkerOpacity);
+        invisibleMarkers[mapPath].push(key);
+    } else {
+        event.target.setOpacity(1.0);
+        invisibleMarkers[mapPath].splice(invisibleMarkers[mapPath].indexOf(key), 1);
+    }
+    localStorage[mapPath] = JSON.stringify(invisibleMarkers[mapPath]);
+};
+window.updatePills = function (event, dataKey) {
+    if (event.target && event.target.options.opacity === 1.0) {
+        markerCount[dataKey] = (markerCount[dataKey] + 1) || 1;
+    } else {
+        markerCount[dataKey] = (markerCount[dataKey] - 1) || 0;
+    }
+    $('ul.key:not(.controls) > li:not(.none) > i.' + dataKey + ' ~ :last').text(markerCount[dataKey]);
+};
+window.resetInvisibleMarkers = function () {
+    var mapKey = 'markers-' + map_path + '-hidden';
+    invisibleMarkers[mapKey] = [];
+    localStorage[mapKey] = JSON.stringify(invisibleMarkers[mapKey]);
+    location.reload();
+};
+window.icons = {};
+window.markers = {};
+window.invisibleMarkers = {};
+window.markerCount = {};
+window.notes = {};
+var icons = window.icons;
+var markers = window.markers;
+var invisibleMarkerOpacity = 0.25;
+window.processData = function (data) {
+    var mapKey = 'markers-' + map_path + '-hidden';
+    if (!localStorage[mapKey]) {
+        localStorage[mapKey] = JSON.stringify([]);
+    }
+    invisibleMarkers[mapKey] = JSON.parse(localStorage[mapKey]);
+    var notesKey = 'notes' + map_path;
+    if (!localStorage[notesKey]) {
+        localStorage[notesKey] = JSON.stringify([]);
+    }
+    notes[map_path] = JSON.parse(localStorage[notesKey]);
+    Object.keys(data).forEach(function (dataKey) {
+        var items = data[dataKey];
+        var groupItems = [];
+        items.forEach(function (item) {
+            if (item.popupTitle == null) {
+                item.popupTitle = item.label;
+            }
+            item.coords.forEach(function (coord) {
+                groupItems.push(createMarker(coord, icons[dataKey], item.label, '<h1>' + item.popupTitle + '</h1>' + item.popup, dataKey));
+            });
+        });
+        markers[dataKey] = L.layerGroup(groupItems);
+    });
+};
+icons.note_marker = L.icon({iconUrl: '../files/images/icons/note_marker.png', iconSize: [23, 28]});
+icons.abandoned = L.icon({iconUrl: '../files/images/icons/abandoned.png', iconSize: [30, 30]});
+icons.alchemy = L.icon({iconUrl: '../files/images/icons/alchemy.png', iconSize: [20, 28]});
+icons.armourer = L.icon({iconUrl: '../files/images/icons/armourer.png', iconSize: [24, 34]});
+icons.armourerstable = L.icon({iconUrl: '../files/images/icons/armourerstable.png', iconSize: [30, 27]});
+icons.banditcamp = L.icon({iconUrl: '../files/images/icons/banditcamp.png', iconSize: [29, 30]});
+icons.barber = L.icon({iconUrl: '../files/images/icons/barber.png', iconSize: [30, 30]});
+icons.hansebase = L.icon({iconUrl: '../files/images/icons/hansebase.png', iconSize: [29, 30]});
+icons.blacksmith = L.icon({iconUrl: '../files/images/icons/blacksmith.png', iconSize: [27, 30]});
+icons.brothel = L.icon({iconUrl: '../files/images/icons/brothel.png', iconSize: [28, 26]});
+icons.entrance = L.icon({iconUrl: '../files/images/icons/entrance.png', iconSize: [28, 27]});
+icons.grindstone = L.icon({iconUrl: '../files/images/icons/grindstone.png', iconSize: [30, 26]});
+icons.guarded = L.icon({iconUrl: '../files/images/icons/guarded.png', iconSize: [23, 34]});
+icons.gwent = L.icon({iconUrl: '../files/images/icons/gwent.png', iconSize: [24, 30]});
+icons.harbor = L.icon({iconUrl: '../files/images/icons/harbor.png', iconSize: [27, 30]});
+icons.herbalist = L.icon({iconUrl: '../files/images/icons/herbalist.png', iconSize: [25, 28]});
+icons.hidden = L.icon({iconUrl: '../files/images/icons/hidden.png', iconSize: [23, 34]});
+icons.innkeep = L.icon({iconUrl: '../files/images/icons/tavern.png', iconSize: [26, 30]});
+icons.kid = L.icon({iconUrl: '../files/images/icons/kid.png', iconSize: [28, 30]});
+icons.monsterden = L.icon({iconUrl: '../files/images/icons/monsterden.png', iconSize: [30, 27]});
+icons.monsternest = L.icon({iconUrl: '../files/images/icons/monsternest.png', iconSize: [23, 30]});
+icons.notice = L.icon({iconUrl: '../files/images/icons/notice.png', iconSize: [23, 28]});
+icons.pid = L.icon({iconUrl: '../files/images/icons/pid.png', iconSize: [24, 34]});
+icons.pop = L.icon({iconUrl: '../files/images/icons/pop.png', iconSize: [27, 30]});
+icons.poi = L.icon({iconUrl: '../files/images/icons/poi.png', iconSize: [28, 28]});
+icons.shopkeeper = L.icon({iconUrl: '../files/images/icons/merchant.png', iconSize: [21, 30]});
+icons.signalfire = L.icon({iconUrl: '../files/images/icons/signalfire.png', iconSize: [17, 34]});
+icons.signpost = L.icon({iconUrl: '../files/images/icons/fasttravel.png', iconSize: [27, 34]});
+icons.smugglers = L.icon({iconUrl: '../files/images/icons/smugglers.png', iconSize: [28, 30]});
+icons.sidequests = L.icon({iconUrl: '../files/images/icons/sidequests.png', iconSize: [10, 30]});
+icons.contracts = L.icon({iconUrl: '../files/images/icons/tempcontract.png', iconSize: [18, 29]});
+icons.vineyardinfestation = L.icon({iconUrl: '../files/images/icons/vineyardinfestation.png', iconSize: [28, 32]});
+icons.vintnercontract = L.icon({iconUrl: '../files/images/icons/vintnercontract.png', iconSize: [23, 34]});
+icons.spoils = L.icon({iconUrl: '../files/images/icons/spoils.png', iconSize: [25, 28]});
--- public/resources/js/map.js
+++ public/resources/js/map.js
@@ -1,12 +1,45 @@
 $(document).ready(function () {
     // declare Map object
-    let map = L.map('map').setView([0, 0],3);
+    let map = L.map('map').setView([0, 0], 3);
     L.tileLayer('/resources/img/map/{z}/{x}/{y}.png', {
         minZoom: 3,
         maxZoom: 6,
-        zoomSnap: 0.2,
         noWrap: true,
 
     }).addTo(map);
+
+    var collectibleTest = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [24, 24]});
+    var marker = L.marker([0, 0], {icon: collectibleTest}).addTo(map);
+    marker.bindPopup();
+
+    marker.on('click', function (e) {
+        console.log(marker.getLatLng().toString());
+        marker.getPopup().setContent('Coord : ' + marker.getLatLng().toString()).openOn(map);
+    });
+
+
+    window.icons = {};
+    window.markers = {};
+    var icons = window.icons;
+    var markers = window.markers;
+
+    icons.collectibleTest = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [38, 95]});
+    icons.collectdouble = L.icon({iconUrl: '/resources/img/marker-icon.png', iconSize: [38, 95]});
+    var iconsLength = Object.keys(icons).length; // obtenir length du json array icons
+
+
+// mapping json to array
+    var arr = $.map(icons, function (el) {
+        return el
+    });
+
+    for (var i = 0; i < iconsLength; i++) {
+        // console.log(arr[i].options);
+        var x = arr[i].options.iconSize[0];
+        var y = arr[i].options.iconSize[1];
+        var url = arr[i].options.iconUrl;
+        // console.log(L.marker([x, y]).addTo(map));
+    }
+
 
 });
