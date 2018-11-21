91a9d58df428f416f5648c0ce688a04a83b6a304
Morgan <nicol.morgan@outlook.fr>
2018-11-21 11:10:11 +0100

    Balisage boulot

--- public/resources/js/map.js
+++ public/resources/js/map.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     // declare Map object
-    let map = L.map('map').setView([0, 0], 3);
+    let map = L.map('map').setView([0, 0],3);
     L.tileLayer('/resources/img/map/{z}/{x}/{y}.png', {
         minZoom: 3,
         maxZoom: 6,
-        continousWorld: false,
+        zoomSnap: 0.2,
         noWrap: true,
+
     }).addTo(map);
 
 });
