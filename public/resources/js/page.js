ffdac46f3878b130acc775768a0a0ffaac9b1141
Morgan <nicol.morgan@outlook.fr>
2018-11-22 15:27:36 +0100

    Style! & style

--- public/resources/js/page.js
+++ public/resources/js/page.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
-    // Remove ads
-    $('a[href="http://leafletjs.com"]').closest('.leaflet-bottom').remove();
+    // REMOVE ZONE
+    $('a[href="http://leafletjs.com"]').closest('.leaflet-bottom').remove(); // Remove ads
+    $('.leaflet-control-layers-toggle').remove(); //  image layer dont on n'a pas besoin
 
+    // GESTION DE LA CONTROL BAR
     const controlBar = $('.leaflet-control-container');
     const zoom = $('.leaflet-left');
     controlBar.addClass('leaflet-control-layers-expanded');
-
     // Déplace la div avec les groupes vers le menu de gauche
     // PUIS re-déplace une sous partie de ce groupe (le Zoom) vers la Map
     $('#menu-panel').append(controlBar);
-    $('#map').append(zoom)
+    $('#map').append(zoom);
 
 
-    $('.leaflet-control-layers-toggle').remove(); // layer image toogle
-
+    const label = $('.leaflet-control-layers-overlays label');
+    label.addClass('disabled');
 });
