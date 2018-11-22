e068859edad7564f8701cebe64be2a54ff1e81a8
Morgan <nicol.morgan@outlook.fr>
2018-11-22 10:25:52 +0100

    Un peu de JS magique pour déplacer les éléments de la top bar de controle zoom/groups

--- public/resources/js/page.js
+++ public/resources/js/page.js
@@ -2,5 +2,14 @@ $(document).ready(function () {
     // Remove ads
     $('a[href="http://leafletjs.com"]').closest('.leaflet-bottom').remove();
 
-    $('.leaflet-control-container').addClass('leaflet-control-layers-expanded');
+    const controlBar = $('.leaflet-control-container');
+    const zoom = $('.leaflet-left');
+    controlBar.addClass('leaflet-control-layers-expanded');
+
+    // Déplace la div avec les groupes vers le menu de gauche
+    // PUIS re-déplace une sous partie de ce groupe (le Zoom) vers la Map
+    $('#menu-panel').append(controlBar);
+    $('#map').append(zoom)
+
+
 });
